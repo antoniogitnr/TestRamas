f21565ccee408830eb4e535882e758cb1d491cc9
antoniogitnr <antonior.pkr@gmail.com>
2023-09-19 20:10:34 -0500

    se modifica codigo para que se puedan agregar clases

--- singleton.py
+++ singleton.py
@@ -1,4 +1,4 @@
-class CreateInstance:
+""" class CreateInstance:
     __instance = None
     def __new__(cls):
         if CreateInstance.__instance is None:
@@ -10,4 +10,30 @@ if __name__ =='__main__':
     nuevainstancia = CreateInstance()
     nuevainstancia2 = CreateInstance()
 
+    print(nuevainstancia is nuevainstancia2) """
+    
+def singleton(cls):
+    __instance = dict()
+    def wrap(*args,**kwargs):
+        if cls not in __instance:
+            print('Nueva instancia')
+            __instance[cls] = cls(*args,**kwargs)
+        return __instance[cls]
+    return wrap
+
+@singleton
+class PrimerClase():
+    pass
+
+@singleton
+class SegundaClase():
+    pass
+
+if __name__ == '__main__':
+    nuevainstancia = PrimerClase()
+    nuevainstancia2 = PrimerClase()
     print(nuevainstancia is nuevainstancia2)
+    
+    instanciaNueva = SegundaClase()
+    instanciaNueva2 = SegundaClase()
+    print(instanciaNueva is instanciaNueva2)
